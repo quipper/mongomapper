5220001d5022116aadeb78966a9ef661a0177371
Nurahmadie <nurahmadie@gmail.com>
2020-03-11 03:10:09 +0700

    [ci skip] Add info about pushAll removal in mongo3.6

--- lib/mongo_mapper/plugins/modifiers.rb
+++ lib/mongo_mapper/plugins/modifiers.rb
@@ -90,6 +90,19 @@ module MongoMapper
           end
         end
 
+        # from: https://docs.mongodb.com/manual/release-notes/3.6-compatibility/#remove-pushall-update-operator
+        #
+        # Remove $pushAll Update Operator
+        #
+        # MongoDB 3.6 removes the deprecated $pushAll operator. The operator has been deprecated since 2.4.
+        # Instead of $pushAll, use the $push operator with the $each modifier. For example:
+        #
+        # ```
+        # db.students.update(
+        #   { name: "joe" },
+        #   { $push: { scores: { $each: [ 90, 92, 85 ] } } }
+        # )
+        # ```
         def deprecate_push_all(modifier, updates)
           return [modifier, updates] unless modifier == '$pushAll'
           ['$push', updates.inject({}) { |accu, (k, v)| accu.tap { |h| h[k] = { '$each' => v } } } ]
